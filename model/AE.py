7a03b836148cf8c908c79cf0322f796efa563be8
msskx <2690438874@qq.com>
2024-03-18 17:21:09 +0800

    add cross attention

--- model/AE.py
+++ model/AE.py
@@ -4,6 +4,7 @@ from torch import nn
 from model.S import Siamese
 from model.utils import Encoder, Decoder
 from model.vit import ViT
+from model.vit.cross_vit import CrossViT
 
 
 class Autoencoder(nn.Module):
@@ -31,26 +32,47 @@ class Autoencoder(nn.Module):
             pool='cls',
             emb_dropout=0.1
         )
+        self.cross_vit = CrossViT(
+            image_size = 256,
+            num_classes = 512 * 16 * 16,
+            depth = 4,               # number of multi-scale encoding blocks
+            # use same encoder
+            sm_dim = 192,            # high res dimension
+            sm_patch_size = 16,      # high res patch size (should be smaller than lg_patch_size)
+            sm_enc_depth = 2,        # high res depth
+            sm_enc_heads = 8,        # high res heads
+            sm_enc_mlp_dim = 2048,   # high res feedforward dimension
+            #########################################
+            lg_dim = 192,            # low res dimension
+            lg_patch_size = 16,      # low res patch size
+            lg_enc_depth = 2,        # low res depth
+            lg_enc_heads = 8,        # low res heads
+            lg_enc_mlp_dim = 2048,   # low res feedforward dimensions
+
+            cross_attn_depth = 2,    # cross attention rounds
+            cross_attn_heads = 8,    # cross attention heads
+            dropout = 0.1,
+            emb_dropout = 0.1
+        )
         self.decoder = Decoder()
 
     def forward(self, vi, ir):
         # 将红外和可见光塞入vit
-        vit_x = self.vit(torch.cat([vi, ir], axis=1))  # 过trans
+        # vit_x = self.vit(torch.cat([vi, ir], axis=1))  # 过trans
+        vit_x = self.cross_vit(vi,ir) # 将可见光编码
         x = torch.cat([vi, ir], dim=1)
-        # fil = nn.Conv2d(in_channels=2, out_channels=1, kernel_size=1, stride=1)  # convert channel
-        # fil = fil.to('cuda')
         # print(f'初始图像：{x.shape}')
         x1 = self.encoder(vi)
         x2 = self.encoder(ir)
         # print(f'编码器图像：{x.shape}')
-        # x = x + vit_x.view(-1, 512, 16, 16)  # 将编码器和注意力机制的东西融合
+        # 将编码器和注意力机制的东西融合
         x = self.decoder(x1 + x2 + vit_x.view(-1, 512, 16, 16))
         # print(f'解码器图像：{x.shape}')
         return x
 
-cc = Autoencoder()
-
-ins1 = torch.randn(32, 1, 256, 256)
-ins2 = torch.randn(32, 1, 256, 256)
-output = cc(ins1,ins2)
-print(output.shape)
+# cc = Autoencoder()
+#
+# ins1 = torch.randn(32, 1, 256, 256)
+# ins2 = torch.randn(32, 1, 256, 256)
+# output = cc(ins1,ins2)
+# print(output.shape)
--- model/vit/cross_vit.py
+++ model/vit/cross_vit.py
@@ -5,18 +5,21 @@ import torch.nn.functional as F
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 
+
 # helpers
 
 def exists(val):
     return val is not None
 
+
 def default(val, d):
     return val if exists(val) else d
+
 
 # feedforward
 
 class FeedForward(nn.Module):
-    def __init__(self, dim, hidden_dim, dropout = 0.):
+    def __init__(self, dim, hidden_dim, dropout=0.):
         super().__init__()
         self.net = nn.Sequential(
             nn.LayerNorm(dim),
@@ -26,40 +29,43 @@ class FeedForward(nn.Module):
             nn.Linear(hidden_dim, dim),
             nn.Dropout(dropout)
         )
+
     def forward(self, x):
         return self.net(x)
+
 
 # attention
 
 class Attention(nn.Module):
-    def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
+    def __init__(self, dim, heads=8, dim_head=64, dropout=0.):
         super().__init__()
-        inner_dim = dim_head *  heads
+        inner_dim = dim_head * heads
         self.heads = heads
         self.scale = dim_head ** -0.5
 
         self.norm = nn.LayerNorm(dim)
-        self.attend = nn.Softmax(dim = -1)
+        self.attend = nn.Softmax(dim=-1)
         self.dropout = nn.Dropout(dropout)
 
-        self.to_q = nn.Linear(dim, inner_dim, bias = False)
-        self.to_kv = nn.Linear(dim, inner_dim * 2, bias = False)
+        self.to_q = nn.Linear(dim, inner_dim, bias=False)
+        self.to_kv = nn.Linear(dim, inner_dim * 2, bias=False)
 
         self.to_out = nn.Sequential(
             nn.Linear(inner_dim, dim),
             nn.Dropout(dropout)
         )
 
-    def forward(self, x, context = None, kv_include_self = False):
+    def forward(self, x, context=None, kv_include_self=False):
         b, n, _, h = *x.shape, self.heads
         x = self.norm(x)
         context = default(context, x)
 
         if kv_include_self:
-            context = torch.cat((x, context), dim = 1) # cross attention requires CLS token includes itself as key / value
+            context = torch.cat((x, context),
+                                dim=1)  # cross attention requires CLS token includes itself as key / value
 
-        qkv = (self.to_q(x), *self.to_kv(context).chunk(2, dim = -1))
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = h), qkv)
+        qkv = (self.to_q(x), *self.to_kv(context).chunk(2, dim=-1))
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=h), qkv)
 
         dots = einsum('b h i d, b h j d -> b h i j', q, k) * self.scale
 
@@ -70,17 +76,18 @@ class Attention(nn.Module):
         out = rearrange(out, 'b h n d -> b n (h d)')
         return self.to_out(out)
 
+
 # transformer encoder, for small and large patches
 
 class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout=0.):
         super().__init__()
         self.layers = nn.ModuleList([])
         self.norm = nn.LayerNorm(dim)
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
-                Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout),
-                FeedForward(dim, mlp_dim, dropout = dropout)
+                Attention(dim, heads=heads, dim_head=dim_head, dropout=dropout),
+                FeedForward(dim, mlp_dim, dropout=dropout)
             ]))
 
     def forward(self, x):
@@ -88,6 +95,7 @@ class Transformer(nn.Module):
             x = attn(x) + x
             x = ff(x) + x
         return self.norm(x)
+
 
 # projecting CLS tokens, in the case that small and large patch tokens have different dimensions
 
@@ -106,6 +114,7 @@ class ProjectInOut(nn.Module):
         x = self.project_out(x)
         return x
 
+
 # cross attention transformer
 
 class CrossTransformer(nn.Module):
@@ -114,44 +123,47 @@ class CrossTransformer(nn.Module):
         self.layers = nn.ModuleList([])
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
-                ProjectInOut(sm_dim, lg_dim, Attention(lg_dim, heads = heads, dim_head = dim_head, dropout = dropout)),
-                ProjectInOut(lg_dim, sm_dim, Attention(sm_dim, heads = heads, dim_head = dim_head, dropout = dropout))
+                ProjectInOut(sm_dim, lg_dim, Attention(lg_dim, heads=heads, dim_head=dim_head, dropout=dropout)),
+                ProjectInOut(lg_dim, sm_dim, Attention(sm_dim, heads=heads, dim_head=dim_head, dropout=dropout))
             ]))
 
     def forward(self, sm_tokens, lg_tokens):
-        (sm_cls, sm_patch_tokens), (lg_cls, lg_patch_tokens) = map(lambda t: (t[:, :1], t[:, 1:]), (sm_tokens, lg_tokens))
+        (sm_cls, sm_patch_tokens), (lg_cls, lg_patch_tokens) = map(lambda t: (t[:, :1], t[:, 1:]),
+                                                                   (sm_tokens, lg_tokens))
 
         for sm_attend_lg, lg_attend_sm in self.layers:
-            sm_cls = sm_attend_lg(sm_cls, context = lg_patch_tokens, kv_include_self = True) + sm_cls
-            lg_cls = lg_attend_sm(lg_cls, context = sm_patch_tokens, kv_include_self = True) + lg_cls
+            sm_cls = sm_attend_lg(sm_cls, context=lg_patch_tokens, kv_include_self=True) + sm_cls
+            lg_cls = lg_attend_sm(lg_cls, context=sm_patch_tokens, kv_include_self=True) + lg_cls
 
-        sm_tokens = torch.cat((sm_cls, sm_patch_tokens), dim = 1)
-        lg_tokens = torch.cat((lg_cls, lg_patch_tokens), dim = 1)
+        sm_tokens = torch.cat((sm_cls, sm_patch_tokens), dim=1)
+        lg_tokens = torch.cat((lg_cls, lg_patch_tokens), dim=1)
         return sm_tokens, lg_tokens
+
 
 # multi-scale encoder
 
 class MultiScaleEncoder(nn.Module):
     def __init__(
-        self,
-        *,
-        depth,
-        sm_dim,
-        lg_dim,
-        sm_enc_params,
-        lg_enc_params,
-        cross_attn_heads,
-        cross_attn_depth,
-        cross_attn_dim_head = 64,
-        dropout = 0.
+            self,
+            *,
+            depth,
+            sm_dim,
+            lg_dim,
+            sm_enc_params,
+            lg_enc_params,
+            cross_attn_heads,
+            cross_attn_depth,
+            cross_attn_dim_head=64,
+            dropout=0.
     ):
         super().__init__()
         self.layers = nn.ModuleList([])
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
-                Transformer(dim = sm_dim, dropout = dropout, **sm_enc_params),
-                Transformer(dim = lg_dim, dropout = dropout, **lg_enc_params),
-                CrossTransformer(sm_dim = sm_dim, lg_dim = lg_dim, depth = cross_attn_depth, heads = cross_attn_heads, dim_head = cross_attn_dim_head, dropout = dropout)
+                Transformer(dim=sm_dim, dropout=dropout, **sm_enc_params),
+                Transformer(dim=lg_dim, dropout=dropout, **lg_enc_params),
+                CrossTransformer(sm_dim=sm_dim, lg_dim=lg_dim, depth=cross_attn_depth, heads=cross_attn_heads,
+                                 dim_head=cross_attn_dim_head, dropout=dropout)
             ]))
 
     def forward(self, sm_tokens, lg_tokens):
@@ -161,24 +173,25 @@ class MultiScaleEncoder(nn.Module):
 
         return sm_tokens, lg_tokens
 
+
 # patch-based image to token embedder
 
-class ImageEmbedder(nn.Module):
+class ImageEmbedder(nn.Module):  # 图像编码层
     def __init__(
-        self,
-        *,
-        dim,
-        image_size,
-        patch_size,
-        dropout = 0.
+            self,
+            *,
+            dim,  # I want to need dim
+            image_size,  # Input image_size
+            patch_size,
+            dropout=0.
     ):
         super().__init__()
         assert image_size % patch_size == 0, 'Image dimensions must be divisible by the patch size.'
-        num_patches = (image_size // patch_size) ** 2
-        patch_dim = 3 * patch_size ** 2
+        num_patches = (image_size // patch_size) ** 2  # example patch_size=16  =>256//16 **2
+        patch_dim = 1 * patch_size ** 2
 
         self.to_patch_embedding = nn.Sequential(
-            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1 = patch_size, p2 = patch_size),
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_size, p2=patch_size),
             nn.LayerNorm(patch_dim),
             nn.Linear(patch_dim, dim),
             nn.LayerNorm(dim)
@@ -189,74 +202,77 @@ class ImageEmbedder(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, img):
-        x = self.to_patch_embedding(img)
+        x = self.to_patch_embedding(img)  # image to patch
         b, n, _ = x.shape
 
-        cls_tokens = repeat(self.cls_token, '() n d -> b n d', b = b)
+        cls_tokens = repeat(self.cls_token, '() n d -> b n d', b=b)
         x = torch.cat((cls_tokens, x), dim=1)
         x += self.pos_embedding[:, :(n + 1)]
 
         return self.dropout(x)
 
+
 # cross ViT class
 
 class CrossViT(nn.Module):
     def __init__(
-        self,
-        *,
-        image_size,
-        num_classes,
-        sm_dim,
-        lg_dim,
-        sm_patch_size = 12,
-        sm_enc_depth = 1,
-        sm_enc_heads = 8,
-        sm_enc_mlp_dim = 2048,
-        sm_enc_dim_head = 64,
-        lg_patch_size = 16,
-        lg_enc_depth = 4,
-        lg_enc_heads = 8,
-        lg_enc_mlp_dim = 2048,
-        lg_enc_dim_head = 64,
-        cross_attn_depth = 2,
-        cross_attn_heads = 8,
-        cross_attn_dim_head = 64,
-        depth = 3,
-        dropout = 0.1,
-        emb_dropout = 0.1
+            self,
+            *,
+            image_size,
+            num_classes,
+            sm_dim,
+            lg_dim,
+            sm_patch_size=12,
+            sm_enc_depth=1,
+            sm_enc_heads=8,
+            sm_enc_mlp_dim=2048,
+            sm_enc_dim_head=64,
+            lg_patch_size=16,
+            lg_enc_depth=4,
+            lg_enc_heads=8,
+            lg_enc_mlp_dim=2048,
+            lg_enc_dim_head=64,
+            cross_attn_depth=2,
+            cross_attn_heads=8,
+            cross_attn_dim_head=64,
+            depth=3,
+            dropout=0.1,
+            emb_dropout=0.1
     ):
         super().__init__()
-        self.sm_image_embedder = ImageEmbedder(dim = sm_dim, image_size = image_size, patch_size = sm_patch_size, dropout = emb_dropout)
-        self.lg_image_embedder = ImageEmbedder(dim = lg_dim, image_size = image_size, patch_size = lg_patch_size, dropout = emb_dropout)
+        self.sm_image_embedder = ImageEmbedder(dim=sm_dim, image_size=image_size, patch_size=sm_patch_size,
+                                               dropout=emb_dropout)
+        self.lg_image_embedder = ImageEmbedder(dim=lg_dim, image_size=image_size, patch_size=lg_patch_size,
+                                               dropout=emb_dropout)
 
         self.multi_scale_encoder = MultiScaleEncoder(
-            depth = depth,
-            sm_dim = sm_dim,
-            lg_dim = lg_dim,
-            cross_attn_heads = cross_attn_heads,
-            cross_attn_dim_head = cross_attn_dim_head,
-            cross_attn_depth = cross_attn_depth,
-            sm_enc_params = dict(
-                depth = sm_enc_depth,
-                heads = sm_enc_heads,
-                mlp_dim = sm_enc_mlp_dim,
-                dim_head = sm_enc_dim_head
+            depth=depth,
+            sm_dim=sm_dim,
+            lg_dim=lg_dim,
+            cross_attn_heads=cross_attn_heads,
+            cross_attn_dim_head=cross_attn_dim_head,
+            cross_attn_depth=cross_attn_depth,
+            sm_enc_params=dict(
+                depth=sm_enc_depth,
+                heads=sm_enc_heads,
+                mlp_dim=sm_enc_mlp_dim,
+                dim_head=sm_enc_dim_head
             ),
-            lg_enc_params = dict(
-                depth = lg_enc_depth,
-                heads = lg_enc_heads,
-                mlp_dim = lg_enc_mlp_dim,
-                dim_head = lg_enc_dim_head
+            lg_enc_params=dict(
+                depth=lg_enc_depth,
+                heads=lg_enc_heads,
+                mlp_dim=lg_enc_mlp_dim,
+                dim_head=lg_enc_dim_head
             ),
-            dropout = dropout
+            dropout=dropout
         )
 
         self.sm_mlp_head = nn.Sequential(nn.LayerNorm(sm_dim), nn.Linear(sm_dim, num_classes))
         self.lg_mlp_head = nn.Sequential(nn.LayerNorm(lg_dim), nn.Linear(lg_dim, num_classes))
 
-    def forward(self, img):
-        sm_tokens = self.sm_image_embedder(img)
-        lg_tokens = self.lg_image_embedder(img)
+    def forward(self, img1, img2):  # 修改成双输入
+        sm_tokens = self.sm_image_embedder(img1)  # 不同尺度编码
+        lg_tokens = self.lg_image_embedder(img2)
 
         sm_tokens, lg_tokens = self.multi_scale_encoder(sm_tokens, lg_tokens)
 
